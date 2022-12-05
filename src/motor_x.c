4d0cc67d255c86ceeb4693947c865f6cd68326de
Awais Tahir <awaistahir29@gmail.com>
2022-12-05 18:15:32 +0100

    Functionlities of MotorX has added

--- src/motor_x.c
+++ src/motor_x.c
@@ -1,6 +1,8 @@
+#include <bits/types/struct_timeval.h>
 #include <curses.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/select.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
@@ -9,11 +11,26 @@
 #include <fcntl.h>
 #include <errno.h>
 
+int x;
+
 int main(int argc, char const *argv[])
 {
     
+    float random_error;
     float x_min = 00.00;
-    float x_max = 38.00;
+    float x_max = 39.00;
+
+    float position = 0;             //position of the X motor
+    float max_x = 1;                //maximum position of X motor
+    float movement_distance = 1.00; //the amount of movement made after receiving a command
+    float movement_time = 1;        //the amount of seconds needed to do the movement
+
+    float movement;
+
+    fd_set readfds;
+    struct timeval timeout;
+
+    
 
     // Path to the named pipe
     char *motorX_fifo = "/tmp/motorX_fifo";
@@ -41,53 +58,89 @@ int main(int argc, char const *argv[])
         return 6;
     }
 
+    
+
     printf("Opened\n");
     
     while(TRUE) {
-        int x;
-        float c;
+        
+        FD_ZERO(&readfds);
+    
+        FD_SET(fd_X, &readfds);
+        FD_SET(fd_insp, &readfds);
+
+        //generating a small random error between -0.02 and 0.02
+        random_error = (float)(-20 + rand() % 40) / 1000;
+        /*
         int d = read(fd_X, &x, sizeof(x));
         if (d == -1){
             printf("Error in reading from pipe\n");
             return 2;
         }
-
-        switch(x){
+        */
+        switch(select(FD_SETSIZE + 1, &readfds, NULL, NULL, &timeout)){
             case 0:
-                printf("Stopping the MotorX\n");
-                break;
+                switch(x){
+                    case 0:
+                        printf("Stopped MotorX\n");
+                        int a = write(fd_insp, &position, sizeof(float));
+                        if (a == -1){
+                            printf("Error Occured writing on insp");
+                            return 5;
+                            }
+                        sleep(movement_time);
+                        break;
+                    case -1:
+                        printf("Decreasing the speed of MotorX\n");
+                        position -= movement;
+                        int aa = write(fd_insp, &position, sizeof(float));
+                        if (aa == -1){
+                            printf("Error Occured writing on insp");
+                            return 5;
+                            }
+                        sleep(movement_time);
+                        break;
+                    case 1:
+                        printf("Increasing the speed of MotorX\n");
+                        movement = movement_distance;
+                        if(position + movement > x_max){
+                            position = x_max;
+                            int b = write(fd_insp, &position, sizeof(float));
+                            if (b == -1){
+                                printf("Error Occured writing on insp");
+                                return 5;
+                            }
+                        sleep(movement_time);
+                        }
+                        else{
+                        position += movement;
+                        int ab = write(fd_insp, &position, sizeof(float));
+                        if (ab == -1){
+                            printf("Error Occured writing on insp");
+                            return 5;
+                            }
+                        }
+                        sleep(movement_time);
+                        break;
+                    }
+                    break;
             case -1:
-                printf("Decreasing the speed of MotorX\n");
-                /*
-                for(int i = x_min; i == 0; i--){
-                    x_min--;
-                    printf("The Value is: %.2f\n", x_min);
-                    
-                    int wr = write(fd_insp, &x_min, sizeof(float));
-                    if(wr == -1){
-                        printf("Error Writing in the fifo\n");
-                        return 7;
-                    }
-                }
-                */
+                printf("Error Occured");
                 break;
-            case 1:
-                printf("Increasing the speed of MotorX\n");
-                for(int i = x_min; i <= x_max; i++){
-                    x_min++;
-                    //n = n + 1;
-                    printf("The Value is: %.2f\n", x_min);
-                    
-                    int wr = write(fd_insp, &x_min, sizeof(float));
-                    if(wr == -1){
-                        printf("Error Writing in the fifo\n");
-                        return 7;
-                    }
-                    sleep(1);
+            default:
+                if (FD_ISSET(fd_X, &readfds))
+            {
+
+                read(fd_X, &x, sizeof(x));
+            }
+            if (FD_ISSET(fd_insp, &readfds))
+            {
+                read(fd_insp, &x, sizeof(x));
+                //strcpy(last_input_command, "");
                 }
-                break;
+            break; 
         }
-}
+    }
     
     // Closing all opened file descripters and unlinking the fifo files
     close(fd_X);
